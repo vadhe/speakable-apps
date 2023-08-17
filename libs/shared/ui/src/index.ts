537680081a2324b6357e1f8d10cbb36535484874
vadhe <aldyvadhe@gmail.com>
2023-08-17 20:10:19 +0800

    create button component

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/card/card';
+export * from './lib/card/card';
 export * from './lib/search/search';
+export * from './lib/button/button';
---
+++ libs/shared/ui/src/lib/button/button.tsx
@@ -0,0 +1,19 @@
+import { MouseEvent, ReactNode } from 'react';
+
+export interface ButtonProps {
+  children?: ReactNode;
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+export function Button({ onClick, children }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex justify-center bg-secondary w-full mt-6 rounded-full py-2"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
